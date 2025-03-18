f89d067c110865a220c306dff76415891179d1d5
Nico52105 <Nico52105@gmail.com>
2025-03-17 19:18:44 -0500

    se agrega logica para crear el arbol del flujo

--- DiseñadorFlujos/EditorFlujo.js
+++ DiseñadorFlujos/EditorFlujo.js
@@ -30,14 +30,53 @@ class EditorFlujo extends HTMLElement {
     link.rel = 'stylesheet';
     link.href = window.location.href + '/EditarFlujo.css';
     div.appendChild(link);
+
+    let indexNodoSeleccionado=0;
+    let nodoRaiz="Inicio";
+    let nodoBuscado=["Inicio"];
     
-    for (var i = 0; i < ObjetoFlujo.Flujo.length; i++) {
-      const divNodo = document.createElement('div');
-      divNodo.innerHTML = ObjetoFlujo.Flujo[i].Nombre;
-      div.appendChild(divNodo);
-    }
     
+    for (var i = 0; i < nodoBuscado.length; i++) {
+      const divNivel = document.createElement('div');
+      divNivel.classList.add('nivel');
+      divNivel.setAttribute('nodo',nodoBuscado[indexNodoSeleccionado-1]);
+      div.appendChild(divNivel);
+      
+      for (var j = 0; j < ObjetoFlujo.Flujo.length; j++) {
+        if (indexNodoSeleccionado<nodoBuscado.length && ObjetoFlujo.Flujo[j].Nombre == "") {
+          const divNodo = document.createElement('div');
+          divNivel.appendChild(divNodo);
+          
+          const divNombreNodo=document.createElement('div');
+          divNombreNodo.innerHTML = ObjetoFlujo.Flujo[j].Nombre;
+          divNodo.appendChild(divNombreNodo);
+          
+          //for (var k = 0; k < ObjetoFlujo.Flujo[j].Respuestas.length; k++) {
+          //  const divMensajesNodo = document.createElement('div');
+          //  divMensajesNodo.innerHTML = ObjetoFlujo.Flujo[j].Respuestas[k];
+          //  divNodo.appendChild(divMensajesNodo);
+          //}
+          //
+          //for (var k = 0; k < ObjetoFlujo.Flujo[j].OpcionesTexto.length; k++) {
+          //  const divOpcionesNodo = document.createElement('div');
+          //  divOpcionesNodo.innerHTML = ObjetoFlujo.Flujo[j].OpcionesTexto[k];
+          //  divNodo.appendChild(divOpcionesNodo);
+          //}
+        }
+        else{
+          for (var k = 0; k < ObjetoFlujo.Flujo[j].EnlacesPermitidos.length; k++) {
+              if (ObjetoFlujo.Flujo[j].EnlacesPermitidos[k].NombreInteraccion.indexOf(nodoBuscado[i]) >= 0){
+                if(nodoBuscado.indexOf(ObjetoFlujo.Flujo[j].Nombre)==-1)
+                nodoBuscado.push(ObjetoFlujo.Flujo[j].Nombre);
+              }
+            }
+          }
+        indexNodoSeleccionado++;
+        }
+      }
+      
     const template = document.createElement('template');
+    console.log(div.outerHTML);
     template.innerHTML = div.outerHTML;
     return template;
   }
