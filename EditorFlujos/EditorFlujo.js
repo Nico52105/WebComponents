9770359d913a6827ffa1fb0d80c163f53ba48231
Nicolas Fiquitiva Segura <Nico52105@gmail.com>
2025-03-18 02:05:03 -0500

    se ajusta la logica para crear el arbol

--- EditorFlujos/EditorFlujo.js
+++ EditorFlujos/EditorFlujo.js
@@ -1,56 +1,61 @@
-let Etiqueta='editor-flujo';
+let Etiqueta = 'editor-flujo';
 class EditorFlujo extends HTMLElement {
   Flujo;
-  
+
   constructor() {
     super();
     this.attachShadow({ mode: "open" })
   }
-  
-  static get observedAttributes(){
+
+  static get observedAttributes() {
     return ["flujo"]
   }
-  
-  attributeChangedCallback(attr,oldVal,newVal){
+
+  attributeChangedCallback(attr, oldVal, newVal) {
     switch (attr) {
       case "flujo":
         this.Flujo = newVal;
-      break;
+        break;
     }
   }
-  
+
   getTemplate() {
-    let ObjetoFlujo=JSON.parse(this.Flujo);
+    let ObjetoFlujo = JSON.parse(this.Flujo);
     console.log(ObjetoFlujo.Flujo);
-    
+
     const div = document.createElement('div');
     div.classList.add('tarjeta');
-    
+
     const link = document.createElement('link');
     link.rel = 'stylesheet';
-    link.href = window.location.href + '/EditarFlujo.css';
+    link.href = window.location.href + 'EditorFlujo.css';
     div.appendChild(link);
 
-    let indexNodoSeleccionado=0;
-    let nodoRaiz="Inicio";
-    let nodoBuscado=["Inicio"];
-    
-    
-    for (var i = 0; i < nodoBuscado.length; i++) {
+    let indexNodoSeleccionado = 0;
+    let nodoPadre;
+    let nodosBuscados = ["Inicio"];
+    let proximosNodosBuscados = [];
+
+
+    for (var i = 0; i < nodosBuscados.length; i++) {
+      console.log("nodosBuscados");
+      console.log(nodosBuscados);
+
       const divNivel = document.createElement('div');
       divNivel.classList.add('nivel');
-      divNivel.setAttribute('nodo',nodoBuscado[indexNodoSeleccionado-1]);
-      div.appendChild(divNivel);
       
+      div.appendChild(divNivel);
+
       for (var j = 0; j < ObjetoFlujo.Flujo.length; j++) {
-        if (indexNodoSeleccionado<nodoBuscado.length && ObjetoFlujo.Flujo[j].Nombre == "") {
+        if (nodosBuscados.indexOf(ObjetoFlujo.Flujo[j].Nombre)!=-1) {
+          
           const divNodo = document.createElement('div');
           divNivel.appendChild(divNodo);
-          
-          const divNombreNodo=document.createElement('div');
+
+          const divNombreNodo = document.createElement('div');
           divNombreNodo.innerHTML = ObjetoFlujo.Flujo[j].Nombre;
           divNodo.appendChild(divNombreNodo);
-          
+
           //for (var k = 0; k < ObjetoFlujo.Flujo[j].Respuestas.length; k++) {
           //  const divMensajesNodo = document.createElement('div');
           //  divMensajesNodo.innerHTML = ObjetoFlujo.Flujo[j].Respuestas[k];
@@ -63,28 +68,37 @@ class EditorFlujo extends HTMLElement {
           //  divNodo.appendChild(divOpcionesNodo);
           //}
         }
-        else{
+        else {
           for (var k = 0; k < ObjetoFlujo.Flujo[j].EnlacesPermitidos.length; k++) {
-              if (ObjetoFlujo.Flujo[j].EnlacesPermitidos[k].NombreInteraccion.indexOf(nodoBuscado[i]) >= 0){
-                if(nodoBuscado.indexOf(ObjetoFlujo.Flujo[j].Nombre)==-1)
-                nodoBuscado.push(ObjetoFlujo.Flujo[j].Nombre);
+            if (ObjetoFlujo.Flujo[j].EnlacesPermitidos[k].NombreInteraccion.indexOf(nodosBuscados[indexNodoSeleccionado])!=-1) {
+              console.log("Validar que no exista el nodo"); 
+              console.log(ObjetoFlujo.Flujo[j].Nombre);              
+              if (proximosNodosBuscados.indexOf(ObjetoFlujo.Flujo[j].Nombre) == -1) {
+                proximosNodosBuscados.push(ObjetoFlujo.Flujo[j].Nombre);
+                console.log("proximosNodosBuscados");
+                console.log(proximosNodosBuscados);
               }
             }
           }
-        indexNodoSeleccionado++;
         }
       }
-      
+      indexNodoSeleccionado++;
+      if(nodosBuscados.length==indexNodoSeleccionado){
+        nodosBuscados = proximosNodosBuscados;
+        proximosNodosBuscados = [];
+        indexNodoSeleccionado = 0;
+      }    
+    }
     const template = document.createElement('template');
     console.log(div.outerHTML);
     template.innerHTML = div.outerHTML;
     return template;
   }
-  
+
   render() {
     this.shadowRoot.appendChild(this.getTemplate().content.cloneNode(true));
   }
-  
+
   connectedCallback() {
     this.render();
   }
