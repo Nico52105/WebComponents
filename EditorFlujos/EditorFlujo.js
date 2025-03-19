5f2826045be0fb175b020c1cafbc2d3d61557e58
Nicolas Fiquitiva Segura <Nico52105@gmail.com>
2025-03-19 00:37:33 -0500

    se ajusta mapeo del diagrama de flujo

--- EditorFlujos/EditorFlujo.js
+++ EditorFlujos/EditorFlujo.js
@@ -32,19 +32,10 @@ class EditorFlujo extends HTMLElement {
     div.appendChild(link);
 
     let arbol={nodos:[],enlaces:{}};
-    for (let i = 0; i < ObjetoFlujo.Flujo.length; i++) {
-      const divNodo = document.createElement('div');
-      const spanNombreNodo = document.createElement('span');
-      spanNombreNodo.innerHTML = ObjetoFlujo.Flujo[i].Nombre;
-      divNodo.appendChild(spanNombreNodo);
-      div.appendChild(divNodo);
+    for (let i = 0; i < ObjetoFlujo.Flujo.length; i++) {      
       arbol.nodos.push(ObjetoFlujo.Flujo[i].Nombre);      
       for (let j = 0; j < ObjetoFlujo.Flujo[i].EnlacesPermitidos.length; j++) {
         for (let k = 0; k < ObjetoFlujo.Flujo[i].EnlacesPermitidos[j].NombreInteraccion.length; k++) {
-          console.log(ObjetoFlujo.Flujo[i].EnlacesPermitidos[j].NombreInteraccion[k]);
-          const spanEnlaces = document.createElement('span');
-          spanEnlaces.innerHTML = ObjetoFlujo.Flujo[i].EnlacesPermitidos[j].NombreInteraccion[k];
-          divNodo.appendChild(spanEnlaces);
           if(arbol.enlaces[ObjetoFlujo.Flujo[i].Nombre]===undefined){
             arbol.enlaces[ObjetoFlujo.Flujo[i].Nombre]=[];
           }
@@ -54,12 +45,16 @@ class EditorFlujo extends HTMLElement {
     }
     console.log(arbol);
     let estructura = [["Inicio"]];
+    let hijosEstructura = [];
     let nodosAgregados = ["Inicio"];
     for (let i = 0; i < estructura.length; i++) {
       let nivel=[];
+      hijosEstructura.push([]);
       for (let j = 0; j < estructura[i].length; j++) {
+        let hijosNodo=0;
         for (let k = 0; k < arbol.nodos.length; k++) {
           if(arbol.enlaces[arbol.nodos[k]].indexOf(estructura[i][j])>=0){
+            hijosNodo++;
             if(nodosAgregados.indexOf(arbol.nodos[k])<0){
               nodosAgregados.push(arbol.nodos[k]);
               nivel.push(arbol.nodos[k]);
@@ -69,12 +64,15 @@ class EditorFlujo extends HTMLElement {
             } 
           }
         }
+        hijosEstructura[i].push(hijosNodo);
       }
       if(nivel.length>0){
         estructura.push(nivel);
       }           
     }
-    console.log(estructura);
+    console.table(estructura);
+    console.table(hijosEstructura);
+
     const template = document.createElement('template');
     console.log(div.outerHTML);
     template.innerHTML = div.outerHTML;
